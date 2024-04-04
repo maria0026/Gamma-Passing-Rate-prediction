74182336954eb6cf689f21f6a9597757f6242fe4
maria0026 <m.waligorska@student.uw.edu.pl>
2024-04-05 01:52:35 +0200

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,6 +1,6 @@
 import pydicom
 
-def normalize_image(image, reference=True, evaluation=False):
+def normalize_ref_image(image):
     #Load DICOM image
     dicom_data = pydicom.dcmread(image)
     #dicom structure:
@@ -11,17 +11,25 @@ def normalize_image(image, reference=True, evaluation=False):
     array_dist = dicom_data.pixel_array
     size = array_dist.shape
     
-    if reference and slope is not None and intercept is not None:
+    if slope is not None and intercept is not None:
         array_dist = (array_dist * slope + intercept)
-        return array_dist, slope, intercept, size
-    
-    elif evaluation and slope is not None and intercept is not None:
-        exposure_sequence = dicom_data[(0x3002, 0x0030)].value
-        meterset_exposure = exposure_sequence[0][(0x3002, 0x0032)].value
-        if meterset_exposure is not None:
-            array_dist = (array_dist * slope + intercept) / meterset_exposure
-            return array_dist, slope, intercept, meterset_exposure, size
-        else:
-            return array_dist, slope, intercept, None, size
+        return array_dist, size
     else:
-        return array_dist, None, None, None, size
+        return None, None
+    
+def normalize_eval_image(image):
+    dicom_data = pydicom.dcmread(image)
+    intercept = dicom_data[(0x0028, 0x1052)].value
+    slope = dicom_data[(0x0028, 0x1053)].value
+    array_dist = dicom_data.pixel_array
+    size = array_dist.shape
+    exposure_sequence = dicom_data[(0x3002, 0x0030)].value
+    meterset_exposure = exposure_sequence[0][(0x3002, 0x0032)].value
+       
+    if slope is not None and intercept is not None and meterset_exposure is not None:
+        array_dist = (array_dist * slope + intercept) / meterset_exposure
+        return array_dist, size
+    else:
+        return None, None
+    
+    
