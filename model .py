26e16ba4e55795c268b9844ad73c00cb3a2e2aaf
maria0026 <m.waligorska@student.uw.edu.pl>
2024-04-05 01:52:38 +0200

    Update model .py

--- model .py
+++ model .py
@@ -1,7 +1,6 @@
-#first attempt- give only reference and gamma passing rate as input
-#read data_for_nn.csv and create a dataframe
-df = pd.read_csv('data_for_nn.csv', sep=',')
-print(df)
+#first attempt- give only reference and gamma passing rate as an input
+
+
 
 
 def map_to_class(gamma_value):
