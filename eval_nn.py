d332a01a73cd52b9681d9c109924294b1136cf10
maria0026 <m.waligorska@student.uw.edu.pl>
2024-04-26 01:15:39 +0200

    Create eval_nn.py

---
+++ eval_nn.py
@@ -0,0 +1,13 @@
+
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = correct / total
+print(f'Test Accuracy: {accuracy:.4f}')
