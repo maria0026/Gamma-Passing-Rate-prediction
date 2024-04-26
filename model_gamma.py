fdff9e8592013d6ad981c469cb6fb2970ef06700
maria0026 <m.waligorska@student.uw.edu.pl>
2024-04-26 02:07:59 +0200

    Update model_gamma.py

--- model_gamma.py
+++ model_gamma.py
@@ -23,9 +23,10 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 #first nn- give only reference and gamma passing rate as an input; second nn- give reference and evaluation and gamma passing rate as an input
+
 class CustomModel(nn.Module):
-    def init(self, num_classes):
-        super(CustomModel, self).init()
+    def __init__(self, num_classes):
+        super(CustomModel, self).__init__()
         self.resnet = resnet18()
         self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
