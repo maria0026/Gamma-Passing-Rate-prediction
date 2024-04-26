3dc0ed41acdfe7f2a54534fc2ffd3b79ac0ab174
maria0026 <m.waligorska@student.uw.edu.pl>
2024-04-26 02:00:52 +0200

    Update model_gamma.py

--- model_gamma.py
+++ model_gamma.py
@@ -24,40 +24,14 @@ from PIL import Image
 
 #first nn- give only reference and gamma passing rate as an input; second nn- give reference and evaluation and gamma passing rate as an input
 class CustomModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.activation = F.relu
-
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.pool1 = nn.MaxPool2d(2)
-
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(2)
-
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(2)
-
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(512)
-        self.pool4 = nn.MaxPool2d(2)
-
-        self.adaptivepool = nn.AdaptiveAvgPool2d((1,1))
-
-        self.linear1 = nn.Linear(512, 10)
+    def init(self, num_classes):
+        super(CustomModel, self).init()
+        self.resnet = resnet18()
+        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
     def forward(self, x):
-        x = self.pool1(self.activation(self.bn1(self.conv1(x))))
-        x = self.pool2(self.activation(self.bn2(self.conv2(x))))
-        x = self.pool3(self.activation(self.bn3(self.conv3(x))))
-        x = self.pool4(self.activation(self.bn4(self.conv4(x))))
-        x = self.adaptivepool(x)
-        x = torch.flatten(x, 1) 
-        x = self.linear1(x)
-        return x
+        return self.resnet(x)
 
 
 
