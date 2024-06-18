d1c75d28c35398cbcdfd37aa2412048a3b7ba8ed
maria0026 <m.waligorska@student.uw.edu.pl>
2024-06-18 22:55:45 +0200

    Update gamma.py

--- gamma/gamma.py
+++ gamma/gamma.py
@@ -9,6 +9,15 @@ from sklearn.model_selection import train_test_split
 from skimage.transform import resize
 from skimage import exposure
 import time
+import cv2
+from skimage.registration import phase_cross_correlation
+import keras
+from keras.models import Sequential,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras.layers import LeakyReLU
+
 
 #print("wersja", tf.__version__)
 
@@ -28,20 +37,17 @@ txt_files = os.listdir(txt_folder)
 
 def find_dcm_without_pair(reference_files, evaluation_files):
     files_without_pair = []
-    i=0
+    
     for file in reference_files:
         pair_name = file.replace("Predicted", "Portal")
         if pair_name not in evaluation_files:
             files_without_pair.append(file)
-            i+=1
 
     for file in evaluation_files:
         pair_name = file.replace("Portal", "Predicted")
         if pair_name not in reference_files:
             files_without_pair.append(file)
-            i+=1
 
-    print("liczba dcm bez pary", i) 
     return files_without_pair
 
 files_without_pair=find_dcm_without_pair(reference_files, evaluation_files)
@@ -95,13 +101,67 @@ def normalize_image(image, reference=True, evaluation=False):
         return array_dist, None, None, None, size
 
         #dist = (dist * slope + intercept) / meterset_exposure
+    
+def draw_maps(ref_dist, eval_dist, gamma):
+    # Plot reference distribution
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 3, 1)
+    plt.imshow(ref_dist, cmap='viridis')
+    plt.title('Reference Distribution')
+    plt.colorbar()
+
+    # Plot evaluation distribution
+    plt.subplot(1, 3, 2)
+    plt.imshow(eval_dist, cmap='viridis')
+    plt.title('Evaluation Distribution')
+    plt.colorbar()
+
+    # Plot gamma distribution
+    plt.subplot(1, 3, 3)
+    plt.imshow(gamma, cmap='coolwarm', vmin=0, vmax=1)
+    plt.title('Gamma Passing Rate')
+    plt.colorbar()
+    plt.tight_layout()
+    plt.show()
+
+def draw_histogram(valid_gamma, gamma_options, pass_ratio, x_ref, y_ref):
+    num_bins = (
+        gamma_options['interp_fraction'] * gamma_options['max_gamma'])
+    bins = np.linspace(0, gamma_options['max_gamma'], int(num_bins) + 1)
+
+    plt.hist(valid_gamma, bins, density=True)
+    #if density is True, y value is probability density; otherwise, it is count in a bin
+    plt.xlim([0, gamma_options['max_gamma']])
+    plt.xlabel('gamma index')
+    plt.ylabel('probability density')
+
+    if gamma_options['local_gamma']:
+        gamma_norm_condition = 'Local gamma'
+    else:
+        gamma_norm_condition = 'Global gamma'
+
+    plt.title(f"Dose cut: {gamma_options['lower_percent_dose_cutoff']}% | {gamma_norm_condition} ({gamma_options['dose_percent_threshold']}%/{gamma_options['distance_mm_threshold']}mm) | Pass Rate(\u03B3<=1): {pass_ratio*100:.2f}% \n ref pts: {len(y_ref)*len(x_ref)} | valid \u03B3 pts: {len(valid_gamma)}")
+    plt.show()
+    #plt.savefig('gamma_hist.png', dpi=300)
 
 
-
-def calculate_gamma(ref, eval, txt, dose_percent_threshold=2, distance_mm_threshold=2, lower_percent_dose_cutoff=7, draw=False) :
+def calculate_gamma(ref, eval, txt, dose_percent_threshold=2, distance_mm_threshold=2, lower_percent_dose_cutoff=5, draw=False) :
     
     ref_dist, slope_ref, intercept_ref, ref_size=normalize_image(ref, reference=True, evaluation=False)
     eval_dist, slope_eval, intercept_eval, meterset_exposure, eval_size=normalize_image(eval, reference=False, evaluation=True)
+    #rezise eval_dist to ref_dist size
+    eval_dist = resize(eval_dist, ref_size)
+    eval_size=eval_dist.shape
+    shift = cv2.phaseCorrelate(ref_dist, eval_dist)
+    #shift= phase_cross_correlation(ref_dist, eval_dist)
+    print(shift)
+    # Apply the shift to the shifted image
+    rows, cols = eval_dist.shape
+    M = np.float32([[1, 0, round(shift[0][0])], [0, 1, round(shift[0][1])]])
+    print(M)
+    #M= np.float32([[1, 0, shift[0]], [0, 1, shift[1]]])
+    eval_dist = cv2.warpAffine(eval_dist, M, (cols, rows))
+    print(np.max(ref_dist), np.max(eval_dist))
 
     #print("max ref", np.max(ref_dist), "max eval", np.max(eval_dist))
 
@@ -111,7 +171,7 @@ def calculate_gamma(ref, eval, txt, dose_percent_threshold=2, distance_mm_thresh
     if slope_eval is None or intercept_eval is None:
         return 0, 0
     #print(np.max(eval_dist)/np.max(ref_dist))
-    #eval_dist = resize(eval_dist, ref_size)  
+
     #print("max ref", np.max(ref_dist), "max eval", np.max(eval_dist))
 
     with open(txt, 'r') as file:   
@@ -168,49 +228,12 @@ def calculate_gamma(ref, eval, txt, dose_percent_threshold=2, distance_mm_thresh
     pass_ratio=round(pass_ratio, 3)
 
     if draw:
-        # Plot reference distribution
-        plt.figure(figsize=(12, 6))
-        plt.subplot(1, 3, 1)
-        plt.imshow(ref_dist, cmap='viridis')
-        plt.title('Reference Distribution')
-        plt.colorbar()
-
-        # Plot evaluation distribution
-        plt.subplot(1, 3, 2)
-        plt.imshow(eval_dist, cmap='viridis')
-        plt.title('Evaluation Distribution')
-        plt.colorbar()
-
-        # Plot gamma distribution
-        plt.subplot(1, 3, 3)
-        plt.imshow(gamma, cmap='coolwarm', vmin=0, vmax=1)
-        plt.title('Gamma Passing Rate')
-        plt.colorbar()
-        plt.tight_layout()
-        plt.show()
-
-        num_bins = (
-            gamma_options['interp_fraction'] * gamma_options['max_gamma'])
-        bins = np.linspace(0, gamma_options['max_gamma'], int(num_bins) + 1)
-
-        plt.hist(valid_gamma, bins, density=True)
-        #if density is True, y value is probability density; otherwise, it is count in a bin
-        plt.xlim([0, gamma_options['max_gamma']])
-        plt.xlabel('gamma index')
-        plt.ylabel('probability density')
-    
-        if gamma_options['local_gamma']:
-            gamma_norm_condition = 'Local gamma'
-        else:
-            gamma_norm_condition = 'Global gamma'
-
-        plt.title(f"Dose cut: {gamma_options['lower_percent_dose_cutoff']}% | {gamma_norm_condition} ({gamma_options['dose_percent_threshold']}%/{gamma_options['distance_mm_threshold']}mm) | Pass Rate(\u03B3<=1): {pass_ratio*100:.2f}% \n ref pts: {len(y_ref)*len(x_ref)} | valid \u03B3 pts: {len(valid_gamma)}")
-        plt.show()
-        #plt.savefig('gamma_hist.png', dpi=300)
+        draw_maps(ref_dist, eval_dist, gamma)
+        draw_histogram(valid_gamma, gamma_options, pass_ratio, x_ref, y_ref)
 
     return pass_ratio, gamma_txt
 
-def calculate_gamma_for_df(df, dose_percent_threshold, distance_mm_threshold, lower_percent_dose_cutoff):
+def calculate_gamma_for_df(df, dose_percent_threshold, distance_mm_threshold, lower_percent_dose_cutoff, draw=False):
     pass_ratios = []
     gamma_txts = []
 
@@ -220,7 +243,7 @@ def calculate_gamma_for_df(df, dose_percent_threshold, distance_mm_threshold, lo
         txt = row['txt']
         print(ref, eval, txt)
         
-        pass_ratio, gamma_txt = calculate_gamma(ref, eval, txt, dose_percent_threshold=dose_percent_threshold, distance_mm_threshold=distance_mm_threshold, lower_percent_dose_cutoff=lower_percent_dose_cutoff)
+        pass_ratio, gamma_txt = calculate_gamma(ref, eval, txt, dose_percent_threshold=dose_percent_threshold, distance_mm_threshold=distance_mm_threshold, lower_percent_dose_cutoff=lower_percent_dose_cutoff, draw=draw)
         print("Pass ratio:", pass_ratio, "Gamma txt:", gamma_txt)
         pass_ratios.append(pass_ratio)
         gamma_txts.append(gamma_txt)
@@ -231,9 +254,9 @@ def calculate_gamma_for_df(df, dose_percent_threshold, distance_mm_threshold, lo
 
     print(df)
     #export df['pass_ratio', 'gamma_txt'] to csv
-    df[['pass_ratio','gamma_txt']].to_csv('gamma_results.csv', sep='\t', index=False)
+    df[['pass_ratio','gamma_txt']].to_csv('gamma_results5.csv', sep='\t', index=False)
 
-#calculate_gamma_for_df(df, dose_percent_threshold=2, distance_mm_threshold=2, lower_percent_dose_cutoff=10)
+calculate_gamma_for_df(df, dose_percent_threshold=2, distance_mm_threshold=2, lower_percent_dose_cutoff=5, draw=True)
 
 #first attempt- give only reference and gamma passing rate as input
 #read data_for_nn.csv and create a dataframe
@@ -288,18 +311,46 @@ def map_to_class(gamma_value):
         return 9
     else:
         return None
-
-
+'''
 df['class'] = df['gamma_txt'].apply(map_to_class)
 print(df['class'])
+
 df_train, df_test= train_test_split(df, test_size=0.2, random_state=42)
+
 desired_size=(1024,1024)
 X_train = load_and_preprocess_images(df_train['ref'], desired_size)
 Y_train=df_train['class']
 X_test = load_and_preprocess_images(df_test['ref'], desired_size)
 Y_test=df_test['class']
-#print(X_train.shape)
 
+train_Y_one_hot = to_categorical(Y_train)
+test_Y_one_hot = to_categorical(Y_test)
+train_X,valid_X,train_label,valid_label = train_test_split(X_train, train_Y_one_hot, test_size=0.2, random_state=13)
+
+batch_size = 64
+epochs = 20
+num_classes = 10
+#print(X_train.shape)
+fashion_model = Sequential()
+fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(1024,1024,1),padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+fashion_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))                  
+fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Flatten())
+fashion_model.add(Dense(128, activation='linear'))
+fashion_model.add(LeakyReLU(alpha=0.1))                  
+fashion_model.add(Dense(num_classes, activation='softmax'))
+fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+fashion_model.summary()
+fashion_train = fashion_model.fit(X_train, train_Y_one_hot, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+test_eval = fashion_model.evaluate(X_test, test_Y_one_hot, verbose=0)
+'''
+'''
 #sieć- obrazek liczba
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=desired_size),
@@ -315,9 +366,49 @@ model.fit(X_train, Y_train, epochs=10)
 test_loss, test_acc = model.evaluate(X_train,  Y_train, verbose=2)
 
 print('\nTest accuracy:', test_acc)
-
+'''
 
 '''
+
+ref='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/reference/270144-Predicted-Dose-e_MIEDNICA-3-plan4.dcm'
+eval='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/evaluation/270144-Portal-Dose-e_MIEDNICA-3-plan4.dcm'
+txt='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/txt/270144-e_MIEDNICA-3-plan4.txt'
+
+ref='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/reference/4160526-Predicted-Dose-CZERNIAK-2-plan1.dcm'
+eval='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/evaluation/4160526-Portal-Dose-CZERNIAK-2-plan1.dcm'
+txt='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/txt/4160526-CZERNIAK-2-plan1.txt'
+'''
+def shift_image(ref_dist, eval_dist):
+    ref_dist, ref_slope, ref_intercept, ref_size =normalize_image(ref_dist, reference=True, evaluation=False)
+    eval_dist, eval_slope, eval_intercept, meterset_exposure, eval_size =normalize_image(eval_dist, reference=False, evaluation=True)
+
+    # Find the shift using phase correlation
+    shift = cv2.phaseCorrelate(ref_dist, eval_dist)
+
+    # Apply the shift to the shifted image
+    rows, cols = eval_dist.shape
+    M = np.float32([[1, 0, shift[0]], [0, 1, shift[1]]])
+    eval_dist_shifted = cv2.warpAffine(eval_dist, M, (cols, rows))
+
+    return ref_dist, eval_dist_shifted
+
+'''
+#ref='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/reference/8045699-Predicted-Dose-eMIGDALEK-2-plan1.dcm'
+#eval='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/evaluation/8045699-Portal-Dose-eMIGDALEK-2-plan1.dcm'
+#txt='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/txt/8045699-eMIGDALEK-2-plan1.txt'
+
+#ref='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/reference/12524-Predicted-Dose-PIERS LW WW-2-plan1.dcm'
+#eval='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/evaluation/12524-Portal-Dose-PIERS LW WW-2-plan1.dcm'
+#txt='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/txt/12524-PIERS LW WW-2-plan1.txt'
+
+#ref='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/reference/884040-Predicted-Dose-BLIZNA PW SO-4-plan2.dcm'
+#eval='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/evaluation/884040-Portal-Dose-BLIZNA PW SO-4-plan2.dcm'
+#txt='/home/marysia/Documents/GitHub/Gamma-Passing-Rate-prediction/data/txt/884040-BLIZNA PW SO-4-plan2.txt'
+dicom_data = pydicom.dcmread(ref)
+print(dicom_data)
+pass_ratio, gamma_txt=calculate_gamma(ref, eval, txt, dose_percent_threshold=2, distance_mm_threshold=2, lower_percent_dose_cutoff=5, draw=True)
+print(pass_ratio, gamma_txt)
+
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
